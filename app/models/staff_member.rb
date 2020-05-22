71e25b968ec9b5d119cdd64c43e78af8072ce17d
YusukeMancity <yusuke1548@gmail.com>
2020-05-22 09:32:58 +0000

    ブランチ間に差異が生まれたため

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,4 +1,6 @@
 class StaffMember < ApplicationRecord
+  has_many :events, class_name: "StaffEvent", dependent: :destroy
+  
   def password=(raw_password)
     if raw_password.kind_of?(String) #kind.of?メソッドで渡された値がString型かどうか判断している
       self.hashed_password = BCrypt::Password.create(raw_password)
